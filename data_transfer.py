a4ba484aa6a913df127c9a136063388c47231df0
ada-vk825 <varun.kokkiligadda25@imperial.ac.uk>
2026-04-17 22:15:50 +0100

    Add data_migration to mongodb

---
+++ data_transfer.py
@@ -0,0 +1,77 @@
+import os
+import sys
+import json
+import pandas as pd
+import numpy as np
+import pymongo
+import certifi
+from dotenv import load_dotenv
+from networksecurity.logging.logger import logging
+from networksecurity.exceptionhandling.exception import CustomException
+
+load_dotenv()  # Load environment variables from .env file
+
+MONGO_BD_URL = os.getenv("MONGO_DB_URL")  # Get the MongoDB URL from environment variable
+
+ca = certifi.where()  # Get the path to the certificate authority bundle provided by certifi. This is used to ensure that the connection to MongoDB is secure and trusted.
+
+
+class NetworkDataExtract:
+    def __init__(self):
+        try:
+            pass
+        except Exception as e:
+            logging.error(f"Error occurred in NetworkDataExtract class: {e}")
+            raise CustomException(e, sys)
+        
+    def csv_to_json(self, file_path: str) -> list:
+        '''Converts a CSV file to JSON format.
+        Args: file_path (str): The path to the CSV file to be converted.
+        Returns: list: A list of dictionaries representing the JSON data.
+        '''
+        try:
+            df = pd.read_csv(file_path)
+            df = df.reset_index(drop=True)  # Reset the index of the DataFrame to ensure it starts from 0 and is sequential.
+            records = df.to_dict(orient="records")  # Convert the DataFrame to a list of dictionaries, where each dictionary represents a row of data with column names as keys and corresponding values.
+            logging.info(f"Successfully converted {file_path} to JSON format.")
+            return records
+
+        except Exception as e:
+            logging.error(f"Error occurred while converting CSV to JSON: {e}")
+            raise CustomException(e, sys)
+        
+    
+    def insert_to_mongodb(self, records, database, collection):
+        '''Inserts a list of records into a MongoDB collection.
+        Args: records (list): A list of dictionaries representing the data to be inserted.
+              database (str): The name of the MongoDB database where the data will be inserted.
+              collection (str): The name of the MongoDB collection where the data will be inserted.
+        Returns: None
+        '''
+        try:
+            self.database = database
+            self.collection = collection
+            self.records = records
+
+            self.client = pymongo.MongoClient(MONGO_BD_URL, tlsCAFile=ca)  # Create a MongoDB client using the provided URL and certificate authority file for secure connection.
+            self.database = self.client[self.database]  # Access the specified database.
+            self.collection = self.database[self.collection]  # Access the specified collection within the database.
+            self.collection.insert_many(self.records)  # Insert the list of records into the collection using the insert_many method, which allows for bulk insertion of multiple documents at once.
+            logging.info(f"Successfully inserted records into MongoDB collection: {self.collection.name} in database: {self.database.name}.")
+            return len(self.records)  # Return the number of records inserted.
+
+        except Exception as e:
+            logging.error(f"Error occurred while inserting records into MongoDB: {e}")
+            raise CustomException(e, sys)
+
+
+if __name__ == "__main__":
+    file_path = "Network_Data/phisingData.csv"
+    database = "NetworkSecurityDB"
+    collection = "PhishingData"
+    network_obj = NetworkDataExtract()
+    records = network_obj.csv_to_json(file_path)
+    print(records[:5])
+    inserted_count = network_obj.insert_to_mongodb(records, database, collection)
+    print(f"Total records inserted: {inserted_count}")
+    logging.info(f"Total records inserted: {inserted_count}")
--- networksecurity/exceptionhandling/exception.py
+++ networksecurity/exceptionhandling/exception.py
@@ -6,13 +6,10 @@ class CustomException(Exception):
     def __init__(self, error_message:str, error_detail: sys):
         super().__init__(error_message)
         self.error_message = error_message
-        _, _, exc_tb = error_detail.exc_info() #gives error type, error message and traceback object
-        self.line_number = exc_tb.tb_lineno # gives the line number where the exception occurred
-        self.file_name = exc_tb.tb_frame.f_code.co_filename # gives the file name where the exception occurred
-
+        _, _, exc_tb = error_detail.exc_info()  # gives error type, error message and traceback object
+        self.line_number = exc_tb.tb_lineno  # gives the line number where the exception occurred
+        self.file_name = exc_tb.tb_frame.f_code.co_filename  # gives the file name where the exception occurred
 
     def __str__(self):
         return f"Error occurred in script: {self.file_name} at line number: {self.line_number} with error message: {self.error_message}"
 
-
-    
